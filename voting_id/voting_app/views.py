4198cbb30b5ca22273a7a95dc7001fae5d4967ad
mayankmtg <mayank15056@iiitd.ac.in>
2017-10-27 13:40:54 +0530

    Landing Page Template Provided

--- voting_id/voting_app/views.py
+++ voting_id/voting_app/views.py
@@ -1,6 +1,6 @@
-# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+	context={}
+	return render(request, 'voting_app/index.html', context)
