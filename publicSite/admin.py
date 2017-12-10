5491ab9dc4388e18712cecc2b34b17d6f1b61d6e
Nora Trapp <me@trappdesign.net>
2017-12-10 02:07:52 -0800

    Add match and review tables and do some basic admin stuff

--- publicSite/admin.py
+++ publicSite/admin.py
@@ -2,21 +2,30 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from publicSite.models import Helper, Helpee, Demographic
+from publicSite.models import Helper, Helpee, Demographic, Match, Review
 
 # Register your models here.
 
 class HelperAdmin(admin.ModelAdmin):
-    fields = ['user', 'state', 'city', 'travel', 'money']
+    fields = ['user', 'city', 'state', 'travel', 'money']
+    list_display = ['email', 'availability', 'location']
 
 class HelpeeAdmin(admin.ModelAdmin):
     fields = ['user', 'state', 'city', 'info', 'travel', 'money']
+    list_display = ['email', 'needs', 'location']
 
 class DemographicAdmin(admin.ModelAdmin):
     fields = ['user', 'age_category', 'gender', 'ethnicity']
 
+class MatchAdmin(admin.ModelAdmin):
+    fields = ['helper', 'helpee', 'match_type', 'estimated_dollars']
+    list_display = ['helper_email', 'helpee_email', 'match_type', 'estimated_dollars']
 
+class ReviewAdmin(admin.ModelAdmin):
+    fields = ['match', 'user', 'rating', 'details', 'actual_dollars']
 
 admin.site.register(Helper, HelperAdmin)
-admin.site.register(Helpee, HelpeeAdmin) 
+admin.site.register(Helpee, HelpeeAdmin)
 admin.site.register(Demographic, DemographicAdmin)
+admin.site.register(Match, MatchAdmin)
+admin.site.register(Review, ReviewAdmin)
--- publicSite/models.py
+++ publicSite/models.py
@@ -5,15 +5,35 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
- 
+
 class Helper(models.Model):
     user = models.OneToOneField(User, related_name='helper')
- 
+
     state = models.CharField(max_length=20)
     city = models.CharField(max_length=60)
 
     travel = models.BooleanField()
     money = models.BooleanField()
+
+    def email(self):
+        return self.user.email
+    email.boolean = False
+    email.short_description = 'Email'
+
+    def location(self):
+        return self.city + ', ' + self.state
+    location.boolean = False
+    location.short_description = 'Location'
+
+    def availability(self):
+        available = []
+        if self.travel:
+            available.append('travel')
+        if self.money:
+            available.append('money')
+        return ', '.join(available)
+    availability.boolean = False
+    availability.short_description = 'Availability'
 
 class Helpee(models.Model):
     user = models.OneToOneField(User, related_name='helpee')
@@ -25,9 +45,60 @@ class Helpee(models.Model):
     travel = models.BooleanField()
     money = models.BooleanField()
 
+    def email(self):
+        return self.user.email
+    email.boolean = False
+    email.short_description = 'Email'
+
+    def location(self):
+        return self.city + ', ' + self.state
+    location.boolean = False
+    location.short_description = 'Location'
+
+    def needs(self):
+        needs = []
+        if self.info:
+            needs.append('info')
+        if self.travel:
+            needs.append('travel')
+        if self.money:
+            needs.append('money')
+        return ', '.join(needs)
+    needs.boolean = False
+    needs.short_description = 'Needs'
+
 class Demographic(models.Model):
     user = models.OneToOneField(User, related_name="demographics")
 
     age_category = models.IntegerField()
     gender = models.CharField(max_length=255)
     ethnicity = models.CharField(max_length=255)
+
+class Match(models.Model):
+    helper = models.ForeignKey(User, on_delete=models.CASCADE, related_name='%(class)s_helper')
+    helpee = models.ForeignKey(User, on_delete=models.CASCADE, related_name='%(class)s_helpee')
+
+    match_type = models.CharField(max_length=1, choices=(('t', 'travel'), ('m', 'money')))
+    estimated_dollars = models.IntegerField()
+
+    def helper_email(self):
+        return self.helper.email
+    helper_email.boolean = False
+    helper_email.short_description = 'Helper Email'
+
+    def helpee_email(self):
+        return self.helpee.email
+    helpee_email.boolean = False
+    helpee_email.short_description = 'Helpee Email'
+
+    # helper_review = Review.objects.get(match=self, user=helper)
+    # helpee_review = Review.objects.get(match=self, user=helpee)
+
+class Review(models.Model):
+    match = models.ForeignKey(Match, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    rating = models.IntegerField()
+    details = models.TextField()
+
+    actual_dollars = models.IntegerField()
