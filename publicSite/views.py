f0dbf9d367f58bc2af22edc84f020f1ceb86c456
Nora Trapp <me@trappdesign.net>
2017-12-11 15:29:21 -0800

    Update the home page with pre-launch info on how to contribute

--- publicSite/views.py
+++ publicSite/views.py
@@ -9,7 +9,7 @@ from publicSite.models import Helper, Helpee, Demographic
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', { 'launched': False })
 
 
 
@@ -26,10 +26,10 @@ def submitHelperInfo(request):
         helper = user.helper
     except:
         helper = Helper(user = user)
-    
+
     selections = request.POST.getlist('checks[]')
-    
-    helper.money = 'money' in selections 
+
+    helper.money = 'money' in selections
     helper.travel = 'travel' in selections
     helper.amount_available = request.POST.get('amount', 0)
 
@@ -37,26 +37,26 @@ def submitHelperInfo(request):
 
     return HttpResponseRedirect(reverse('getLocationForm'))
 
-  
-  
+
+
 #NEW - show form to create new helpee
 def getHelpeeForm(request):
     return render(request, "helpeeForm.html")
-    
+
 #CREATE - add new helpee to database
 def submitHelpeeInfo(request):
 
     user = request.user
-    
+
     try:
         helpee = user.helpee
     except:
         helpee = Helpee(user = user)
-    
+
     selections = request.POST.getlist('checks[]')
-    
+
     helpee.info = 'info' in selections
-    helpee.money = 'money' in selections 
+    helpee.money = 'money' in selections
     helpee.travel = 'travel' in selections
     helpee.amount_needed = request.POST.get('amount', None)
     helpee.travel_cost = request.POST.get('travel_cost', None)
@@ -70,35 +70,35 @@ def submitHelpeeInfo(request):
 #NEW - show form to create new location
 def getLocationForm(request):
     return render(request, 'locationForm.html')
-    
+
 #CREATE - add new campground to database
 def submitLocationInfo(request):
-    
+
     user = request.user
-    
+
     try:
         person = user.helper
     except:
         person = user.helpee
-    
+
     person.state = request.POST.get('state', None)
     person.city = request.POST.get('city', None)
     person.save()
-    
+
     return HttpResponseRedirect(reverse('thankYou'))
 
 
 #THANK YOU Page
 def thankYou(request):
-    return render(request, 'thankYou.html')    
+    return render(request, 'thankYou.html')
 
 
 #NEW - show form to create new demographic info
 def getDemographicForm(request):
     return render(request, 'demographicForm.html')
-   
+
 #CREATE - add new demographic info to database
 def submitDemographicInfo(request):
-    
+
     return
-    
+
